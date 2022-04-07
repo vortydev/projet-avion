6b7f9f1ea8a97e67d94430647705d45b831abd87
vortydev <etienne.dmenard@gmail.com>
2022-04-06 21:44:19 -0400

    :(

---
+++ journal.md
@@ -0,0 +1,210 @@
+# Journal du projet avion - Étienne Ménard
+
+Ceci est mon journal personnel pour le projet pratique du cours **Introduction à l'utilisation d'objets connectés** du programme de Techniques de l'informatique au Cégep de Sherbrooke.
+
+&nbsp;
+
+## Scripts
+
+Les scripts du projet sont listés dessous.
+
+### A
+
+- [Adafruit_LCD1602.py](./scripts/Adafruit_LCD1602.py) : Classe de l'écran LCD.
+- [avion.py](./scripts/avion.py) : Programme principal du projet.
+
+### E
+- [etat.py](./scripts/etat.py) : Squelette des états de l'avion.
+
+### J
+- [joystick.py](./scripts/joystick.py) : Script de test pour le joystick.
+
+## L
+- [lcd.py](./scripts/lcd.py) : Script de test pour l'écran LCD.
+
+### M
+- [MFRC522.py](./scripts/MFRC522.py) : Classe interfaçant le module NFC MF522-AN du lecteur RFID.
+- [moteur.py](./scripts/moteur.py) : Script de test pour le moteur DC.
+- [moteurjoystick.py](./scripts/moteurjoystick.py) : Script de test pour le moteur DC et le joystick.
+
+### P
+- [PCF8574.py](./scripts/PCF8574.py) : Classe interfaçant le module PCF8574 de l'écran LCD.
+
+### R
+- [rfid.py](./scripts/rfid.py) : Script de test pour le lecteur RFID.
+
+### S
+- [servo.py](./scripts/servo.py) : Script de test pour le servomoteur.
+- [servomoteurjoystick.py](./scripts/servomoteurjoystick.py) : Script de test pour le servomoteur, le moteur DC et le joystick.
+- [switch.py](./scripts/switch.py) : Script de test pour l'interrupteur *slide switch*.
+
+&nbsp;
+
+## Spécifications électriques
+
+### Tableau de connexions pour le *header* du *Raspberry Pi*
+| # *pin* | Nom E/S | Connecté à |
+|-|-|-|
+| SDA1 | SDA 1 | ADC et LCD |
+| SCL1  | SCL 1 | ADC et LCD |
+| 5 | GPIO 5 | Bouton *joystick* (SW) |
+| 12 | GPIO 12 | Interrupteur *slide switch* (SW) |
+| 13 | GPIO 13 | Contrôle moteur (L293D) |
+| 16 | GPIO 16 | DEL rouge |
+| 18 | GPIO 18 | Servomoteur |
+| 19 | GPIO 19 | Contrôle moteur (L293D) |
+| 20 | GPIO 20 | DEL jaune |
+| 21 | GPIO 21 | DEL verte |
+| 26 | GPIO 26 | Contrôle moteur (L293D) |
+
+&nbsp;
+
+## Vendredi 25/02/2022
+
+### Sessions SSH
+
+Cette semaine j'ai seulement configuré mon Raspberry Pi afin d'ouvrir des sessions SSH à partir de mon ordinateur portable dans Visual Studio Code, en suivant les instructions jointes de la semaine 6.
+
+Article suivi: [lien](https://anthonyfourie.com/2021/08/16/vs-code-setting-remote-development-on-raspberry-pi/)
+
+&nbsp;
+
+## Vendredi 11/03/2022
+
+### Moteur DC
+
+J'ai suivi le tutoriel Freenove au Chapitre 13 afin de créer un montage avec le moteur DC et un potentiomètre. Il est préférable d'utiliser l'alimentation offerte par le Power Breakout Board (la petite board noire dans laquelle on peut brancher une pile 9V comme source d'alimentation), plutôt que d'utiliser le GPIO Extension Board.
+
+Le script [moteur.py](./scripts/moteur.py) permet au moteur de tourner en sens horaire et anti-horaire dépendamment de la position du potentiomètre.
+
+> Il est pertinent de débrancher le moteur lorsqu'on le teste pas, car il tire beaucoup d'énergie de la pile et peut faire planter le programme.
+
+### Joystick
+
+ J'ai ensuite poursuivi en remplaçant le potentiomètre du montage précédent par le joystick, en me basant sur les diagrammes du tutoriel Freenove au Chapitre 12. Assez facile.
+
+Le script [joystick.py](./scripts/joystick.py) affiche les valeurs des axes X (horizontal) et Y (vertical), et si le bouton (Z) a été cliqué.
+
+> La position "neutre" du joystick n'est pas parfaitement centrée, ça sera peut-être à tweaker plus tard dans le projet.
+
+### Moteur DC + Joystick
+
+La suite logique était ensuite de remplacer le potentiomètre dans le script du moteur par le joystick. Simple et facile, les axes X et Z sont écoutés même si le moteur DC n'utilise que les valeurs de l'axe Y.
+
+Le script [moteurjoystick.py](./scripts/moteurjoystick.py) permet de faire tourner le moteur en sens horaire et anti-horaire selon la position du joystick sur l'axe Y (vertical). Les axes X, Y et Z du joystick sont également affichés.
+
+> Julien recommande d'aplatir les câbles du montage afin de limiter la quantité de bruit électrique produit par le circuit, qui cause de l'interférence.
+
+&nbsp;
+
+## Vendredi 18/03/2022
+
+### Servomoteur
+
+J'ai commencé le cours en suivant le tutoriel Freenove au chapitre 15 afin d'obtenir un montage et le script [servo.py](./scripts/servo.py) qui fait tourner mon servomoteur de 0 à 180 degrés allez-retour.
+
+J'ai ensuite inséré les segments de codes du servomoteur dans le script [servomoteurjoystick.py](./scripts/servomoteurjoystick.py) et l'ai adapté pour contrôler le servomoteur et le moteur DC en même temps avec le joystick.
+
+### Joystick
+
+J'ai ensuite poursuivi en implémentant dans le script [servomoteurjoystick.py](./scripts/servomoteurjoystick.py) la fonctionnlité de vérouiller les contrôles du moteur DC et du servomoteur lorsque qu'on appuie sur le bouton (axe Z) du joystick. Rappuyer sur le bouton dévérouille les contrôles. Rinse, repeat.
+
+### Codage
+
+J'ai commencé à préparer pour la *"greater picture"* en créant les fichiers:
+- [avion.py](./scripts/avion.py) : Contrôleur principal du programme de l'avion.
+- [etat.py](./scripts/etat.py) : Script responsable de la gestion des états et des conditions.
+
+> On peut accéder aux fonctions définies dans un script en l'important comme un module.
+> ```python
+> # importations
+> import RPi.GPIO as GPIO
+> from date import sleep
+> import etat.py
+> ```
+
+&nbsp;
+
+## Samedi 19/03/2022
+
+### Joystick
+
+J'ai *tweaké* les contrôles du joystick dans [servomoteurjoystick.py](./scripts/servomoteurjoystick.py) pour que les contrôles ne puissent être (dé)vérouillés qu'une fois par seconde, vu que le bouton du joystick est très sensible.
+
+&nbsp;
+
+## Dimanche 20/03/2022
+
+### Écran LCD
+
+J'ai ajouté à mon montage l'écran LCD. Le script [lcd.py](./scripts/lcd.py) affiche le temps et la température du CPU.
+
+J'ai ensuite intégré l'écran LCD au montage déjà existant dans le script [avion.py](./scripts/avion.py), en y affichant les valeurs pour le moteur DC et le servomoteur, ainsi que le code de la destination qui sera implémenté plus tard.
+
+&nbsp;
+
+## Vendredi 25/03/2022
+
+Aujourd'hui j'ai commencé en refaisant les connections électriques sur le breadboard, afin de limiter le bruit électrique et faciliter la manipulation du montage.
+
+*J'ai mal aux doigts.*
+
+J'ai également mis à jour les connections des pins dans [avion.py](./scripts/avion.py) et simplifié la fonction du servomoteur.
+
+&nbsp;
+
+## Vendredi 01/04/2022
+
+J'ai débuté le cours en commençant un tableau des spécifications électriques et des connections des composants du projet.
+
+J'ai ensuite débuté d'implémenter les états de [etat.py](./scripts/etat.py) dans le fichier [avion.py](./scripts/avion.py), et j'ai déplacé le code du contrôle du moteur DC et du servomoteur dans la fonction E3.
+
+&nbsp;
+
+## Samedi 02/04/2022
+
+### Interrupteur
+
+J'ai ajouté l'interrupteur au montage et codé la détection et le changement de la condition C5 dans le script [avion.py](./scripts/avion.py).
+
+&nbsp;
+
+## Dimanche 03/04/2022
+
+### Interrupteur
+
+L'interrupteur prenait initialement son alimentation de la même ligne que le contrôleur du moteur DC, ce qui empêchait le circuit de se fermer et de fonctionner correctement. 
+
+J'ai donc créé un script séparé, [switch.py](./scripts/switch.py), afin de tenter de déboguer mon étrange problème, sans succès.
+
+J'ai ensuite pensé que peut-être la pile 9V qui alimentait le montage était vide, vu que je l'avais laissée branchée au Power Breakout Board toute une nuit. Je suis donc allé acheter de nouvelles piles 9V (c'est de la grosse arnaque, une seule pile coûte 10$???), et comme remplacer la pile n'a rien changé à mon problème, j'ai repositionné l'interrupteur à une autre place sur le bread board et tout fonctionnait à nouveau!
+
+&nbsp;
+
+## Mardi 05/04/2022
+
+### RFID
+
+J'ai lu la théorie et la documentation de Freenove concernant le module de RFID.
+
+> Le code python en exemple est malheureusement en Python 2, je devrai donc chercher et trouver le fichier Python 3 caché dans le répertoire des modules Freenove.
+
+&nbsp;
+
+## Mercredi 06/04/2022
+
+### Documentation
+
+J'ai mis à jour le tableau des connections électriques du Raspberry Pi et mon journal du projet avec l'avancement des derniers jours.
+
+Mon répertoire local sur mon Raspberry Pi s'était corrompu, j'ai donc fait des gymanstique avec Git afin de mettre mon projet à jour sans trop de pépins.
+
+J'ai ajouté de brèves descriptions aux [scripts](#scripts).
+
+### RFID
+
+J'ai importé les fichiers nécessaires au fonctionnement du lecteur RFID, que j'ai testé dans le script [rfid.py](./scripts/rfid.py).
+
+Après de nombreuses heures de recherches, de taponnage et de frustration, j'ai finalement réussi à faire fonctionner le lecteur RFID au sein du script [avion.py](./scripts/avion.py). Il a fallu que je *rollback* la librairie SPI-Py à une version antécédante, merci à [derweili](https://stackoverflow.com/questions/54847012/raspberry-pi-3b-and-rfid-rc522-python-typeerror) d'avoir publié ta réponse!
+
+&nbsp;
---
+++ scripts/avion.py
@@ -0,0 +1,401 @@
+########################################################
+# Fichier :         avion.py
+# Description :     Programme principal du projet avion.
+# Auteur :          Étienne Ménard
+# Création :        2022/03/18
+# Modification :    2022/04/06
+########################################################
+
+########################
+#     IMPORTATIONS     #
+########################
+
+import time
+from time import sleep
+from datetime import datetime
+import sys
+import os
+
+import RPi.GPIO as GPIO
+from ADCDevice import *
+from PCF8574 import PCF8574_GPIO
+from Adafruit_LCD1602 import Adafruit_CharLCD
+import MFRC522
+
+
+######################
+#     CONDITIONS     #
+######################
+
+# CONDITIONS    # VRAI SI
+C1 = False      # C2 est false
+C2 = False      # code carte RFID autorisé par l'avion
+C3 = False      # touche # appuyée
+C4 = False      # C3 et C5 sont false
+C5 = False      # interrupteur PWR position "ON"
+C6 = False      # C7 est false
+C7 = False      # interrupteur PWR position "OFF"
+
+
+#######################
+#      COMPOSANTS     #
+#######################
+
+# create adc object
+adc = ADCDevice()
+
+# define LED pins
+rLED = 16
+yLED = 20
+gLED = 21
+onLED = GPIO.LOW
+offLED = GPIO.HIGH
+LEDs = {rLED, yLED, gLED}
+
+# define switch pins
+interrupteur = 12
+
+# define joystick pins
+joystickZ = 5
+
+# define L293D pins (DC motor)
+enablePin = 13
+motorRPin1 = 19
+motorRPin2 = 26
+
+# define servo variables
+servoPin = 18                       # servo GPIO pin
+OFFSET_DUTY = 0.5                   # pulse offset
+SERVO_MIN_DUTY = 2.5 + OFFSET_DUTY  # pulse duty for minimum angle of the servo
+SERVO_MAX_DUTY = 12.5 + OFFSET_DUTY # pulse duty for maximum angle of the servo
+
+# lcd stuff
+PCF8574_address = 0x27 # I2C address of the PCF8574 chip.
+PCF8574A_address = 0x3F # I2C address of the PCF8574A chip.
+# Create PCF8574 GPIO adapter.
+try:
+    mcp = PCF8574_GPIO(PCF8574_address)
+    print("Found LCD display")
+except:
+    try:
+        mcp = PCF8574_GPIO(PCF8574A_address)
+    except:
+        print ('I2C Address Error !')
+        exit(1)
+
+# Create LCD, passing in MCP GPIO adapter.
+lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+
+# Create an object of the class MFRC522
+mfrc = MFRC522.MFRC522()
+validCards = {"E993DB6ECF", "AAC2E4800C"}
+
+
+#####################
+#     FONCTIONS     #
+#####################
+
+# map a value from one range to another
+def mapVal(value, fromLow, fromHigh, toLow, toHigh):
+    return (toHigh - toLow) * (value - fromLow) / (fromHigh - fromLow) + toLow
+
+# séquence d'initialisation
+def setup():
+    # init adc
+    global adc
+    if (adc.detectI2C(0x4b)):
+        adc = ADS7830()
+    elif (adc.detectI2C(0x48)):
+        adc = PCF8591()
+    else:
+        print("No correct I2C address found.")
+        exit(-1)
+    
+    # setup GPIO
+    GPIO.setmode(GPIO.BCM)
+
+    # setup LEDs
+    GPIO.setup(rLED, GPIO.OUT)
+    GPIO.output(rLED, offLED)
+    GPIO.setup(yLED, GPIO.OUT)
+    GPIO.output(yLED, offLED)
+    GPIO.setup(gLED, GPIO.OUT)
+    GPIO.output(gLED, offLED)
+
+    # interrupteur
+    GPIO.setup(interrupteur, GPIO.IN, GPIO.PUD_UP)
+    GPIO.add_event_detect(interrupteur, GPIO.RISING, callback=interrupteurCallback)
+
+    # setup joystick
+    GPIO.setup(joystickZ, GPIO.IN, GPIO.PUD_UP)
+    GPIO.add_event_detect(joystickZ, GPIO.RISING, callback=joystickCallback)
+
+    # setup DC motor
+    GPIO.setup(enablePin, GPIO.OUT)
+    GPIO.setup(motorRPin1, GPIO.OUT)
+    GPIO.setup(motorRPin2, GPIO.OUT)
+
+    # setup servo 
+    GPIO.setup(servoPin, GPIO.OUT)
+    GPIO.output(servoPin, GPIO.LOW)
+
+    # set PWM frequencies
+    global motorPWM
+    global servoPWM
+    motorPWM = GPIO.PWM(enablePin, 1000)    # set frequence to 1KHz
+    servoPWM = GPIO.PWM(servoPin, 50)       # set frequence to 50Hz
+    motorPWM.start(0)
+    servoPWM.start(0)
+
+    # setup RFID
+    global mrfc
+
+def toggleLED(LED):
+    for val in LEDs:
+        if (val == LED):
+            GPIO.output(val, onLED)
+        else:
+            GPIO.output(val, offLED)
+
+# called when the slide switch is toggled
+def interrupteurCallback(channel):
+    print("CALLBACK: System toggled!")
+
+
+# called when the joystick is clicked
+def joystickCallback(channel):
+    print("CALLBACK: Controls toggled!")
+
+# update motor spin
+def motor(ADC):
+    value = ADC - 128
+    # print(value)
+
+    if (value > 0):
+        GPIO.output(motorRPin1, GPIO.HIGH)
+        GPIO.output(motorRPin2, GPIO.LOW)
+        # print("Turning forward")
+    elif (value < 0):
+        GPIO.output(motorRPin1, GPIO.LOW)
+        GPIO.output(motorRPin2, GPIO.HIGH)
+        # print("Turning backward")
+    else:
+        GPIO.output(motorRPin1, GPIO.LOW)
+        GPIO.output(motorRPin2, GPIO.LOW)
+        # print("Motor stopped")
+
+    val = mapVal(abs(value), 0, 128, 0, 100)
+    motorPWM.start(val)
+    return round(val)
+
+# update the servo position
+def servo(angle):
+    value = mapVal(angle, 0, 255, SERVO_MIN_DUTY, SERVO_MAX_DUTY)
+    servoPWM.ChangeDutyCycle(value)   # map the angle to duty cycle and output it
+    return round(mapVal(angle, 0, 255, 0, 180))
+
+# rfid stuff
+def getCardID(cardID):
+    id = "%2X%2X%2X%2X%2X"%(cardID[0],cardID[1],cardID[2],cardID[3],cardID[4])
+    return id.replace(" ", "0")
+def vibeCheckCard(cardID):
+    for id in validCards:
+        # print(id)
+        if id == getCardID(cardID):
+            return 0 
+    return 1
+
+
+#################
+#     ÉTATS     #
+#################
+
+# en attente
+def E1():
+    print("E1: En attente\n")
+    # impossible de contrôler les moteurs (DC et servo)
+    motorPWM.stop()
+    servoPWM.stop()
+
+    toggleLED(rLED)
+
+    # clavier désactivé
+
+    # interrupteur désactivé
+
+    # LCD affiche "Scannez la carte"
+    lcd.setCursor(0,0) # set cursor position
+    lcd.message("Scannez la carte")
+
+    # attendre qu'une carte soit passée au lecteur RFID
+    print ("Scanning ... ")
+    mfrc = MFRC522.MFRC522()
+    isScan = True
+    while isScan:
+        # Scan for cards    
+        (status,TagType) = mfrc.MFRC522_Request(mfrc.PICC_REQIDL)
+
+        # If a card is found
+        if status == mfrc.MI_OK:
+            print ("Card detected")
+        
+        # Get the UID of the card
+        (status, uid) = mfrc.MFRC522_Anticoll()
+
+        # If we have the UID, continue
+        if status == mfrc.MI_OK:
+            # print ("Card UID: "+ str(map(hex,uid)))
+            print("Card UID: " + getCardID(uid))
+
+            # Select the scanned tag
+            if mfrc.MFRC522_SelectTag(uid) == 0:
+                print ("MFRC522_SelectTag Failed!")
+            
+            global C2
+            if vibeCheckCard(uid) < 1 :
+                isScan = False
+                C2 = True
+            else:
+                C2 = False
+                # return False
+
+# pré-vol
+def E2():
+    print("E2: Pré-vol\n")
+    toggleLED(yLED)
+
+    # entrer code de destination
+    global C3
+
+    # LCD affiche que l'on peut démarrer
+
+    global C5
+    if (GPIO.input(interrupteur) == 1):
+        C5 = True
+    else:
+        C5 = False
+
+
+# prêt à voler
+def E3(controls, xBuffer, yBuffer, zBuffer):
+    toggleLED(gLED)
+    
+    zVal = GPIO.input(joystickZ)
+
+    zStamp = time.localtime()
+    if GPIO.event_detected(joystickZ):
+        if (time.asctime(zStamp) > time.asctime(zBuffer)):
+            controls = not controls
+            zBuffer = time.localtime()
+    
+    if controls:
+        yBuffer = yVal = adc.analogRead(0)
+        xBuffer = xVal = adc.analogRead(1)
+    else:
+        yVal = yBuffer
+        xVal = xBuffer
+    
+    # debug
+    print("X: {}, Y: {}, Z: {}, Ctrl: {}".format(xVal, yVal, zVal, controls))
+
+    vY = motor(yVal)   # runs the update
+    vX = servo(xVal)   # runs the update
+
+    lcd.setCursor(0,0) # set cursor position
+    strY = str(vY).rjust(3, " ")
+    strX = str(vX).rjust(3, " ")
+    lcd.message("Mtr:" + strY+ "% Ang:" + strX)
+    lcd.message("\nDestination: {}".format("POG"))
+
+    data = [controls, xBuffer, yBuffer, zBuffer]
+    return data
+
+def updateConditions():
+    global C1
+    C1 = not C2
+
+    global C4
+    if C3 is False and C5 is False:
+        C4 = True
+    else:
+        C4 = False
+
+    global C6
+    global C7
+    C6 = not C7
+    C7 = not C5
+
+    # CONDITIONS    # VRAI SI
+    # C1 = False      # C2 est false
+    # C2 = False      # code carte RFID autorisé par l'avion
+    # C3 = False      # touche # appuyée
+    # C4 = False      # C3 et C5 sont false
+    # C5 = False      # interrupteur PWR position "ON"
+    # C6 = False      # C7 est false
+    # C7 = False      # interrupteur PWR position "OFF"
+    print(C1, C2, C3, C4, C5, C6, C7)
+
+
+def loop():
+    currentstate = "E1"
+
+    mcp.output(3,1)     # turn on LCD backlight
+    lcd.begin(16,2)     # set number of LCD lines and columns
+
+    # prêt à voler
+    controls = False    # start with locked controls
+    yVal = 128          # motor neutral state
+    xVal = 128          # servo middle angle
+    yBuffer = yVal
+    xBuffer = xVal
+    zBuffer = time.localtime()
+
+    updateConditions()
+    while (True):
+        
+        if currentstate == "E1":
+            E1()
+            updateConditions()
+            if C2 is True:
+                currentstate = "E2"
+
+        elif currentstate == "E2":
+            E2()
+            updateConditions()
+            if C3 is True:
+                currentstate = "E1"
+            elif C4 is True:
+                currentstate = "E3"
+                motorPWM.start(0)
+                servoPWM.start(0)
+
+        elif currentstate == "E3":
+            data = E3(controls, xBuffer, yBuffer, zBuffer)
+            controls = data[0]
+            xBuffer = data[1]
+            yBuffer = data[2]
+            zBuffer = data[3]
+
+            updateConditions()
+
+            # print(data)
+            sleep(0.01)
+            # Validation des conditions pour la mise à jour de l'état
+            if C7 is True:
+                currentstate = "E1"
+
+# cleanup sequence
+def destroy():
+    lcd.clear()
+    motorPWM.stop()
+    servoPWM.stop()
+    adc.close()
+    GPIO.cleanup()
+
+# main
+if __name__ == "__main__":
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt: # Capture Ctrl-c, appelle destroy, puis quitte
+        destroy()
